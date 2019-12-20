df42a2ebdbb22b273f00c01993de2af319a270f8
Robert Clarke <clarke.john.robert@gmail.com>
2019-12-20 02:07:12 -0500

    Added a JSON error reporting function and some TODOs, removed commented code

--- filedog.py
+++ filedog.py
@@ -29,6 +29,7 @@ def on_created(event):
     file_path, file_name = file_path.rsplit("\\", 1)
     print((file_path, file_name, file_extension))
 
+    #TODO: make this behaviour trigger if a match is found in a regex file
     if file_name == "image0":
         try:
             global INCREMENT_NUMBER
@@ -39,18 +40,13 @@ def on_created(event):
             print("Already exists")
     print()
 
-#def on_deleted(event):
-#    print(f"'{event.src_path}' was deleted!")
-#def on_modified(event):
-#    print(f"'{event.src_path}' has been modified")
-#def on_moved(event):
-#    print(f"'{event.src_path}' was moved to '{event.dest_path}'")
 
 def main():
     FILEDOG_DATA_DIR = f"{FILEDOG_BASE_DIR}\\filedog_settings"
     FILEDOG_TARGETLIST_JSON = f"{FILEDOG_DATA_DIR}\\targets.json"
     FILEDOG_INCREMENT_FILE = f"{FILEDOG_DATA_DIR}\\id_offset.txt"
-
+    
+    print("Starting filedog...")
     if not os.path.exists(FILEDOG_DATA_DIR):
         os.makedirs(FILEDOG_DATA_DIR)
 
@@ -61,37 +57,43 @@ def main():
         with open(FILEDOG_INCREMENT_FILE, "r") as targetfile:
             INCREMENT_NUMBER = int(targetfile.readline())
 
+    #Loading info from JSON setting files
+    #####################################
+    def displayFriendlyJSONErr(jsonError: json.decoder.JSONDecodeError):
+        print(f"Encountered a JSON parsing error for targets.json")
+        print(f"Malformed line is at line {jsonError.lineno}, column {jsonError.colno}: '{jsonError.msg}'")
+        print(f"\nCheck for errors at '{FILEDOG_TARGETLIST_JSON}'")
+        print("\nStopping filedog...")
+
     PATHS_TO_MONITOR = []
     if not os.path.exists(FILEDOG_TARGETLIST_JSON):
         with open(FILEDOG_TARGETLIST_JSON, "w") as targetfile:
-            targetfile.writelines(["[\n", f"\t{FILEDOG_BASE_DIR}\n", "]"])
+            targetfile.writelines(["[\n", "\t\"" + FILEDOG_BASE_DIR.replace("\\","\\\\") +"\"\n", "]"])
         print("Created missing target file, need to populate with paths to watch.")
         print(f"File located at: '{FILEDOG_TARGETLIST_JSON}'", end="\n\n")
         return None
     else:
         import json
-        PATHS_TO_MONITOR = json.load(open(FILEDOG_TARGETLIST_JSON))
+        try:
+            PATHS_TO_MONITOR = json.load(open(FILEDOG_TARGETLIST_JSON))
+        except json.decoder.JSONDecodeError as err:
+            displayFriendlyJSONErr(err)
+            return None
+
+    #TODO: load list of regexes to trigger renaming
 
 
     from watchdog.events import RegexMatchingEventHandler
     patterns = [r".*"]
     ignore_patterns = [""]
-    ignore_directories = False
+    ignore_directories = True
     case_sensitive = False
-    my_event_handler = RegexMatchingEventHandler(ignore_directories=True)#patterns, ignore_patterns, ignore_directories, case_sensitive)
+    my_event_handler = RegexMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
 
-    my_event_handler.on_created = on_created
-    #my_event_handler.on_deleted = on_deleted
-    #my_event_handler.on_modified = on_modified
-    #my_event_handler.on_moved = on_moved
+    my_event_handler.on_created = on_created #TODO: make custom handler more obvious
     
-    
-    #from watchdog.observers import Observer
     from watchdog.observers.read_directory_changes import WindowsApiObserver as Observer
-    #path = "."
     go_recursively = True
-    #my_observer = Observer()
-    #my_observer.schedule(my_event_handler, path, recursive=go_recursively)
 
     OBSERVER_LIST = []
     for path in PATHS_TO_MONITOR:
@@ -102,15 +104,20 @@ def main():
     for ob in OBSERVER_LIST:
         ob.start()
 
+
+    #Main thread waits until finished
     import time
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
+        pass
+    finally:
         for ob in OBSERVER_LIST:
             ob.stop()
             ob.join()
         #do other cleanup behaviours
+        print("Stopping filedog...")
     return None
 
 if __name__ == "__main__":
